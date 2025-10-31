34f69be3fe36169ce8d44014525c497b8b88f66b
remco jonk <remcojonk@hotmail.com>
2025-10-31 12:37:42 +0100

    created home hero section for desktop

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   i18n: {
     locales: ["nl", "en", "fr", "de"],
     defaultLocale: "nl",
   },
+
+  integrations: [react()],
 });
